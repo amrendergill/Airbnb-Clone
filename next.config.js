e5d0fa2ef2e5f11e0b8b3c93036b6078ffff7228
amrendergill <amrender028@gmail.com>
2021-08-29 19:53:18 +0530

    ini

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYW1yZW5kZXJnaWxsIiwiYSI6ImNrc3g5cHk1MTI4MG8yeG4xemo1ZGtxbTQifQ.Dos1GdRsksySsZ1IhZfLvg",
+  },
+};
